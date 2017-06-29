f9304ded14bf871ed91cd99073aeae1109b99e19
shariff6 <shariff.mohammed.a@gmail.com>
2017-06-29 11:28:28 +0300

    added test for numbers divisible by 5

--- spec/ping_pong_spec.rb
+++ spec/ping_pong_spec.rb
@@ -3,6 +3,9 @@ require('ping_pong')
 
 describe('Fixnum#ping_pong') do
   it("changes numbers divisible by 15 to pingpong")do
-  expect(15.ping_pong()).to(eq(["pingpong"]))
+  expect(15.ping_pong()).to(eq(["pong", "pong", "pingpong"]))
+end
+ it ("changes numbers divisible by 5 to pong ") do
+   expect(5.ping_pong()).to(eq(["pong"]))
 end
 end
